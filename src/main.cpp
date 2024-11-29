92c000b03843624edf201c126b282924117bca51
Ayush Pratap Singh <ayushpratap3154@gmail.com>
2024-11-29 17:13:31 +0530

    Looping

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,11 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
+  while(true) {
   // Uncomment this block to pass the first stage
   std::cout << "$ ";
   std::string input;
   std::getline(std::cin, input);
   std::cout<<input<<": command not found\n";
+  }
 }
