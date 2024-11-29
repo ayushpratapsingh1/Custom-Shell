e54a4307cc7502b2a3f73ac831e5257e1b540597
Ayush Pratap Singh <ayushpratap3154@gmail.com>
2024-11-29 17:38:56 +0530

    Exit

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main() {
   std::cout << "$ ";
   std::string input;
   std::getline(std::cin, input);
-  std::cout<<input<<": command not found\n";
   if(input=="exit 0") exit(0);
+  std::cout<<input<<": command not found\n";
   }
 }
