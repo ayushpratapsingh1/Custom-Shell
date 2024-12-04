0c71dfbe4f9a2660d794669974fd8331c3578ec1
Ayush Pratap Singh <ayushpratap3154@gmail.com>
2024-12-04 21:47:10 +0530

    implement cd

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@
 #include <vector>
 #include <fstream>
 #include <filesystem>
+
 using namespace std;
+
 std::string WORKING_DIR = std::filesystem::current_path().string();
+
 std::vector<std::string> split_string(const std::string &s, char delimiter) {
     std::stringstream ss(s);
     std::vector<std::string> tokens;
@@ -15,12 +18,18 @@ std::vector<std::string> split_string(const std::string &s, char delimiter) {
     }
     return tokens;
 }
+
 void handleCd(const std::string& argument) {
-    if (argument[0] == '/' && std::filesystem::exists(argument))
-        WORKING_DIR = argument;
-    else
+    std::filesystem::path new_path = argument[0] == '/' ? argument : WORKING_DIR + '/' + argument;
+
+    if (std::filesystem::exists(new_path) && std::filesystem::is_directory(new_path)) {
+        std::filesystem::current_path(new_path);
+        WORKING_DIR = std::filesystem::current_path().string(); // Update the working directory variable
+    } else {
         std::cout << argument << ": No such file or directory\n";
+    }
 }
+
 void handle_type_command(const std::vector<std::string> &args, const std::vector<std::string> &path) {
     if (args[1] == "echo" || args[1] == "exit" || args[1] == "type" || args[1] == "pwd") {
         std::cout << args[1] << " is a shell builtin\n";
@@ -55,12 +64,10 @@ int main() {
             for (size_t i = 1; i < args.size(); ++i) {
                 std::cout << args[i] << (i == args.size() - 1 ? "\n" : " ");
             }
-        } else if(args[0] == "pwd"){
-            std::string cwd = std::filesystem::current_path();
-            std::string print_cwd = cwd.substr(0, cwd.length());
-            std::cout << print_cwd << "\n";
+        } else if (args[0] == "pwd") {
+            std::cout << WORKING_DIR << "\n"; // Use WORKING_DIR to print the current directory
         } else if (args[0] == "cd" && args.size() > 1) {
-            handleCd(args[1]); 
+            handleCd(args[1]);
         } else if (args[0] == "type" && args.size() > 1) {
             handle_type_command(args, path);
         } else {
